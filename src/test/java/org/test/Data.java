351cc7158335012b56d99fc2093f704ec8beb5ca
pushpamathi <pushpamathijeyam@gmail.com>
2023-02-09 08:09:17 +0530

    checking

--- src/test/java/org/test/Data.java
+++ src/test/java/org/test/Data.java
@@ -3,9 +3,9 @@ package org.test;
 import java.io.File;
 
 import org.baseclass.BaseClass;
+import org.junit.Test;
 
 public class Data extends BaseClass {
-public static void main(String[] args) {
-	File f =new File("https://d.docs.live.net/c4fa98be87a0f646/Book.xlsx")
+		
 }
 }
